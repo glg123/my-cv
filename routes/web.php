c872fbe3a6c4761471ca094e0e657388a0015378
Your Name <you@example.com>
2022-03-12 01:59:00 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('welcome');
+});
 ;
 Route::post('/contact', [UsersController::class, 'contact'])->name('contact');
